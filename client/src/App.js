c40413bb2bd22128391fb5c7fd62d0e54e9ba295
Mina Mina <minakhamesi@gmail.com>
2020-10-13 09:02:42 -0400

    homeScreen first version -without router- is done, Product & Rating components Done.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+
+
 
 
 function App() {
   return (
     <>
-    App
+    <Header/>
+      <main className='py-3'>
+      <Container>
+      <HomeScreen />
+      </Container>
+    
+      </main>
+      
+  
+    <Footer/>
     </>
   );
 }
---
+++ client/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Container, Row, Col  } from 'react-bootstrap';
+
+
+const Footer = () => {
+    return (
+        <footer>
+           <Container>
+               <Row>
+                   <Col className='py-3 text-center'>
+                       Copyright &copy; MyAmazon
+                   </Col>
+               </Row>
+           </Container>
+        </footer>
+    )
+}
+
+export default Footer;
