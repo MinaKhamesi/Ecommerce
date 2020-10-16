08856b088c2db727d43bb14408ab434b2402eee1
Mina Mina <minakhamesi@gmail.com>
2020-10-16 09:48:00 -0400

    redux setup done. store created and incorporated to application, redux devtool ready.

---
+++ client/src/store.js
@@ -0,0 +1,15 @@
+import {createStore, applyMiddleware, combineReducers} from 'redux';
+import thunk from 'redux-thunk';
+import {composeWithDevTools} from 'redux-devtools-extension';
+
+const reducer = combineReducers({
+
+});
+
+const initialState = {};
+
+const middlewares = [thunk]
+
+const store = createStore(reducer,initialState, composeWithDevTools(applyMiddleware(...middlewares)));
+
+export default store;
