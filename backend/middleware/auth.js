517398d1117973450d826aabca7d546867b29bad
Mina Mina <minakhamesi@gmail.com>
2020-10-20 19:42:12 -0400

    isAdmin middleWare is done

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -34,3 +34,14 @@ export const protect = asyncHandler(async (req,res,next) =>{
 
  
 })
+
+export const isAdmin = asyncHandler(async (req,res,next) =>{
+    
+    if (req.user && req.user.isAdmin){
+        next();
+    }else{
+        res.status(401);
+        throw new Error('Not authorized as an admin');
+    }
+})
+
