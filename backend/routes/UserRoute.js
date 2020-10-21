621de0bc3cd2c3fc9b0f979af61771d959814c8f
Mina Mina <minakhamesi@gmail.com>
2020-10-21 14:31:17 -0400

    getUserById and EditUser endpoints created for admin only

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import {isAdmin, protect} from '../middleware/auth.js';
-import {authUser,registerUser , getProfile, updateProfile, getUsers, deleteUserById} from '../controllers/userController.js';
+import {authUser,registerUser , getProfile, updateProfile, getUsers, deleteUserById, getUserById, editUser} from '../controllers/userController.js';
 
 
 const router = express.Router();
 
 router.route('/').post(registerUser).get(protect, isAdmin, getUsers);
-router.route('/:id').delete(protect, isAdmin, deleteUserById);
+
+router.route('/:id').get(protect, isAdmin, getUserById).put(protect, isAdmin, editUser).delete(protect, isAdmin, deleteUserById);
 
 router.route('/login').post(authUser);
 
