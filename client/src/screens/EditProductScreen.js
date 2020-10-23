92b50133e2128568f70f8100c596efa4f1663917
Mina Mina <minakhamesi@gmail.com>
2020-10-23 10:41:30 -0400

    upload file frontend done

--- client/src/screens/EditProductScreen.js
+++ client/src/screens/EditProductScreen.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, {useState, useEffect} from 'react';
 import { Link} from 'react-router-dom';
 import {useDispatch, useSelector} from 'react-redux';
@@ -19,6 +20,7 @@ const EditProductScreen = ({match}) => {
     const [image, setImage] = useState('');
     const [price, setPrice] = useState(0);
     const [countInStock, setCountInStock] = useState(0);
+    const [uploading, setUploading] = useState(false);
     
     const productDetail = useSelector(state=>state.productDetail);
     const {product, loading, error} = productDetail;
@@ -47,6 +49,30 @@ const EditProductScreen = ({match}) => {
     }, [ dispatch,match, product]);
 
 
+
+    const uploadFileHandler = async(e)=>{
+        const file = e.target.files[0];
+        const formData = new FormData();
+        formData.append('image',file);
+
+        setUploading(true);
+
+        try {
+            const config = {
+                headers:{
+                    'Content-Type': 'multipart/form-data'
+                }
+            }
+
+            const { data } = axios.post(`api/upload`, formData, config);
+            setImage(data);
+            setUploading(false);
+        } catch (err) {
+            console.log(err);
+            setUploading(false);
+        }
+
+    }
     const handleUpdate = e =>{
         e.preventDefault();
         dispatch(updateProduct(match.params.id,{name,brand,category,image,description,countInStock,price}))
@@ -67,6 +93,21 @@ const EditProductScreen = ({match}) => {
                         <Form.Label>Name</Form.Label>
                         <Form.Control type='name' placeholder='Enter name' value={name} onChange={e=>setName(e.target.value)}></Form.Control>
                 </Form.Group>
+                <Form.Group controlId='price'>
+                        <Form.Label>price</Form.Label>
+                        <Form.Control type='number' placeholder='Enter price' value={price} onChange={e=>setPrice(e.target.value)}></Form.Control>
+                </Form.Group>
+                <Form.Group controlId='image'>
+                        <Form.Label>Image</Form.Label>
+                        <Form.Control type='text' placeholder='Enter image url' value={image} onChange={e=>setImage(e.target.value)}></Form.Control>
+                </Form.Group>
+                <Form.File id='image-file' label='Choose File' custom onChange={uploadFileHandler}></Form.File>
+                {uploading && <Loader /> }
+                <Form.Group controlId='countInStock'>
+                        <Form.Label>count In Stock</Form.Label>
+                        <Form.Control type='number' placeholder='Enter count In Stock' value={countInStock} onChange={e=>setCountInStock(e.target.value)}></Form.Control>
+                </Form.Group>
+                
                 <Form.Group controlId='brand'>
                         <Form.Label>brand</Form.Label>
                         <Form.Control type='text' placeholder='Enter brand' value={brand} onChange={e=>setBrand(e.target.value)}></Form.Control>
@@ -79,23 +120,6 @@ const EditProductScreen = ({match}) => {
                         <Form.Label>description</Form.Label>
                         <Form.Control type='text' placeholder='Enter Description' value={description} onChange={e=>setDescription(e.target.value)}></Form.Control>
                 </Form.Group>
-                <Form.Group controlId='image'>
-                        <Form.Label>Image</Form.Label>
-                        <Form.Control type='text' placeholder='Enter image' value={image} onChange={e=>setImage(e.target.value)}></Form.Control>
-                </Form.Group>
-                <Form.Group controlId='price'>
-                        <Form.Label>price</Form.Label>
-                        <Form.Control type='number' placeholder='Enter price' value={price} onChange={e=>setPrice(e.target.value)}></Form.Control>
-                </Form.Group>
-                <Form.Group controlId='countInStock'>
-                        <Form.Label>count In Stock</Form.Label>
-                        <Form.Control type='number' placeholder='Enter count In Stock' value={countInStock} onChange={e=>setCountInStock(e.target.value)}></Form.Control>
-                </Form.Group>
-                
-
-                
-
-                
 
                 <Button className='btn-block' type='submit' variant='primary'>
                    Update
