6e9ae807b875bfdb23eb9cf50c61d9ce40b5d55a
Mina Mina <minakhamesi@gmail.com>
2020-10-29 09:35:25 -0400

    createReview redux workflow is done

--- client/src/actions/ProductActions.js
+++ client/src/actions/ProductActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import {PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_DETAIL_REQUEST, PRODUCT_DETAIL_SUCCESS, PRODUCT_DETAIL_FAIL, PRODUCT_DELETE_REQUEST, PRODUCT_DELETE_SUCCESS, PRODUCT_DELETE_FAIL, PRODUCT_DELETE_RESET, PRODUCT_CREATE_FAIL, PRODUCT_CREATE_SUCCESS, PRODUCT_CREATE_REQUEST, PRODUCT_UPDATE_SUCCESS, PRODUCT_UPDATE_FAIL, PRODUCT_UPDATE_REQUEST, PRODUCT_UPDATE_RESET} from '../constants/productConstants';
+import {PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL, PRODUCT_DETAIL_REQUEST, PRODUCT_DETAIL_SUCCESS, PRODUCT_DETAIL_FAIL, PRODUCT_DELETE_REQUEST, PRODUCT_DELETE_SUCCESS, PRODUCT_DELETE_FAIL, PRODUCT_DELETE_RESET, PRODUCT_CREATE_FAIL, PRODUCT_CREATE_SUCCESS, PRODUCT_CREATE_REQUEST, PRODUCT_UPDATE_SUCCESS, PRODUCT_UPDATE_FAIL, PRODUCT_UPDATE_REQUEST, PRODUCT_UPDATE_RESET, PRODUCT_CREATE_REVIEW_REQUEST, PRODUCT_CREATE_REVIEW_SUCCESS, PRODUCT_CREATE_REVIEW_FAIL} from '../constants/productConstants';
 
 export const getProducts = ()=> async (dispatch) =>{
     try {
@@ -110,5 +110,29 @@ export const updateProduct = (id, formData)=> async (dispatch,getState) =>{
     } catch (error) {
         const err = error.response && error.response.data.message ? error.response.data.message : error.message
         dispatch({type:PRODUCT_UPDATE_FAIL,payload:err})
+    }
+}
+
+
+export const createReview = (productId, review)=> async (dispatch,getState) =>{
+    try {
+        
+        dispatch({type:PRODUCT_CREATE_REVIEW_REQUEST});
+
+        const config = {
+            headers:{
+                Authorization:`Bearer ${getState().userLogin.userInfo.token}`,
+                'Content-Type': 'application/json'
+            }
+        }
+
+         await axios.post(`/api/products/${productId}/reviews`,review, config);
+        
+        dispatch({type:PRODUCT_CREATE_REVIEW_SUCCESS});
+        
+        
+    } catch (error) {
+        const err = error.response && error.response.data.message ? error.response.data.message : error.message
+        dispatch({type:PRODUCT_CREATE_REVIEW_FAIL,payload:err})
     }
 }
