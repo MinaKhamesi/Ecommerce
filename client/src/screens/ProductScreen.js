107a9871ce050097b686388326a4d5f40fd48368
Mina Mina <minakhamesi@gmail.com>
2020-10-29 10:26:23 -0400

    Review section and submit review section added to ProductScreen and createReview functionality added

--- client/src/screens/ProductScreen.js
+++ client/src/screens/ProductScreen.js
@@ -1,37 +1,55 @@
 import React, { useEffect, useState} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import {Link} from 'react-router-dom';
-import { Row, Col, ListGroup, Card, Button, ListGroupItem,Image} from 'react-bootstrap';
+import { Row, Col, ListGroup, Card, Button, ListGroupItem,Image, Form, FormGroup, FormLabel, FormControl} from 'react-bootstrap';
 import Rating from '../components/Rating';
 import Loader from '../components/Loader';
 import Message from '../components/Message';
 
 
-import {getProduct} from '../actions/ProductActions';
+import {getProduct, createReview} from '../actions/ProductActions';
+import { PRODUCT_CREATE_REVIEW_RESET } from '../constants/productConstants';
 
 
 const ProductScreen = ({match, history}) => {
 
     const [qty, setQty] = useState(1);
+    const [rating, setRating] = useState(0);
+    const [comment, setComment] = useState('');
 
     const productDetail = useSelector(state=>state.productDetail);
     const {product, error, loading} = productDetail;
 
+    const productReview = useSelector(state=>state.productReview);
+    const { error:reviewError,success:reviewSuccess, loading:reviewLoading} = productReview;
+
+
+    const userLogin = useSelector(state=>state.userLogin);
+    const {userInfo} = userLogin;
+
     const dispatch = useDispatch();
 
     useEffect(() => {
+            dispatch({type:PRODUCT_CREATE_REVIEW_RESET});
+
         dispatch(getProduct(match.params.id));
-    }, [match, dispatch]);
+    }, [match, dispatch,reviewSuccess]);
      
 
     const addToCartHandler = e =>{
         history.push(`/cart/${match.params.id}?qty=${qty}`);
+    }
+
+    const createReviewHandler = (e)=>{
+        e.preventDefault();
+        dispatch(createReview(match.params.id,{rating,comment}));
     }
     
     return (
         <>
            <Link to='/' className="btn btn-light m-3">Go Back</Link>
     {loading ? <Loader/> : error ? <Message variant='danger'>{error}</Message> : (
+        <>
         <Row>
         <Col md={6}>
             <Image src={product.image} alt={product.name} fluid/>
@@ -89,6 +107,47 @@ const ProductScreen = ({match, history}) => {
             </Card>
         </Col>
     </Row>
+    <Row>
+        <Col md={5}>
+            <h3>Reviews</h3>
+            {product.reviews.length===0 ? <Message>No review is submitted for this product</Message> : (<ListGroup variant='flush'>{product.reviews.map(review=><ListGroupItem>
+                <Rating value={review.rating}/>
+                <p>{review.comment}</p>
+            </ListGroupItem>)}</ListGroup>)}
+        </Col>
+    </Row>
+    <Row className='my-4'>
+        <Col md={5}>
+            <h3>Submit a review</h3>
+            {reviewSuccess && <Message variant='success'>Review Added</Message>}
+            {reviewError && <Message variant='danger'>{reviewError}</Message>}
+            {reviewLoading && <Loader/>}
+            {(userInfo && userInfo._id) ? (
+                <Form onSubmit={e=>createReviewHandler(e)}>
+                <FormGroup>
+                    <FormLabel>Rating</FormLabel>
+                    <FormControl as='select' value={rating} onChange={e=>setRating(e.target.value)}>
+                        <option value=''>select ...</option>
+                        <option value='1'>1-Poor</option>
+                        <option value='2'>2-Fair</option>
+                        <option value='3'>3-Good</option>
+                        <option value='4'>4-Very good</option>
+                        <option value='5'>5-Excellent</option>
+                    </FormControl>
+                </FormGroup>
+                <FormGroup>
+                    <FormLabel>Comment</FormLabel>
+                    <FormControl as='textarea' value={comment} onChange={e=>setComment(e.target.value)}></FormControl>
+                </FormGroup>
+                <FormGroup>
+                    <Button className='btn btn-block' onClick={e=>createReviewHandler(e)}>Submit</Button>
+                </FormGroup>
+            </Form>
+            ) : <Message>Please <Link to='/login'>Sign In</Link> to submit your review</Message>}
+            
+        </Col>
+    </Row>
+    </>
     )}
            
         </>
