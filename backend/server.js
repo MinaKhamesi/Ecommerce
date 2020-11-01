4df95974b42cb097cfa34f7b858c925adf0cedcd
Mina Mina <minakhamesi@gmail.com>
2020-11-01 13:24:48 -0500

    prepared for deployment

--- backend/server.js
+++ backend/server.js
@@ -24,9 +24,7 @@ connectDB();
 
 app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.send('API is running')
-})
+
 
 app.use('/api/products',ProductRouter);
 app.use('/api/users',UserRouter);
@@ -37,6 +35,15 @@ app.get('/api/config/paypal',(req,res)=>res.send(process.env.PAYPAL_CLIENT_ID));
 
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname,'/uploads')));
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname,'/client/build')));
+    app.get('*', (req,res)=>res.sendFile(path.resolve(__dirname,'client','build','index.html')));
+}else{
+    app.get('/',(req,res)=>{
+        res.send('API is running')
+    })
+}
 
 app.use(notFound);
 app.use(errorHandler);
