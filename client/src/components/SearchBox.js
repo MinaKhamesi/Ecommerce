cc60fd92ee443a6c0f0c44e5295e3e8eed2bc202
Mina Mina <minakhamesi@gmail.com>
2020-11-02 07:48:03 -0500

    change search botton margin to my-1 because it all skewshed together in mobile

--- client/src/components/SearchBox.js
+++ client/src/components/SearchBox.js
@@ -11,8 +11,8 @@ const SearchBox = ({history}) => {
 
     return (
         <Form onSubmit={handleSearch} inline >
-            <FormControl as='input' placeholder='Search Product...' value={keyword} onChange={e=>setKeyword(e.target.value)} className='mr-1 ml-3'/>
-            <Button as='button' variant='outline-dark'>Search</Button>
+            <FormControl as='input' placeholder='Search Product...' value={keyword} onChange={e=>setKeyword(e.target.value)} className='mr-1 ml-3 ' />
+            <Button as='button' variant='outline-dark' className='my-1'>Search</Button>
         </Form>
     )
 }
